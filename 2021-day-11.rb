13c1dacebf15df26f4401e67121e3a7dfee68691
TyJaYo <tylerjamesyoung@gmail.com>
2021-12-11 20:48:19 -0600

    fixed loop logic day 11 part 1

--- 2021-day-11.rb
+++ 2021-day-11.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 require 'pry'
 
-# INPUT = []
-INPUT = ["5483143223","2745854711","5264556173","6141336146","6357385478","4167524645","2176841721","6882881134","4846848554","5283751526"]
+INPUT = ["8261344656","7773351175","7527856852","1763614673","8674556743","6853382153","4135852388","2846715522","7477425863","4723888888"]
+SINPUT = ["5483143223","2745854711","5264556173","6141336146","6357385478","4167524645","2176841721","6882881134","4846848554","5283751526"]
 ROWSIZE = INPUT.first.size
 ROWCOUNT = INPUT.size
-SLEEPYTIME = 1
+SLEEPYTIME = 0.1
 STEPCOUNT = 100
 CSI = "\e["
 
@@ -24,7 +24,7 @@ class Octomap
     $stdout.write "#{CSI}2J"    # clear screen
     $stdout.write "#{CSI}1;1H"  # move to top left corner
     counter = ROWSIZE
-    @octomap.each do |k,v|
+    @octomap.each do |k, v|
       counter -= 1
       if counter > 0
         $stdout.print v
@@ -33,18 +33,16 @@ class Octomap
         counter = ROWSIZE
       end
     end
-    puts t
+    puts "After #{t} steps..."
     sleep(SLEEPYTIME)
   end
   def step
-    @octomap.each {|k,v| inc(k,v)} # all values += 1
-    while @octomap.any? { |k,v| v > 9 } # while any > 9
-      @octomap.each do |k,v| # flash if > 9
-        if v > 9
-          flash(k)
-        end
-      end
-      @flashers.each do |f| # adjacent to flash += 1
+    @octomap.each_key { |k| inc(k) } # all values += 1
+    flashed = []
+    while 'Tyler' > 'Everyone'
+      flashers = @octomap.select { |k, v| v > 9 && !flashed.include?(k)} .keys  # flash if > 9 and haven't flashed
+      break if flashers.empty?
+      flashers.each do |f| # adjacent to flash += 1
         neighbors = []
         neighbors << [f[0]-1,f[1]-1] # NW
         neighbors << [f[0],f[1]-1]   # N
@@ -55,37 +53,31 @@ class Octomap
         neighbors << [f[0],f[1]+1]   # S
         neighbors << [f[0]+1,f[1]+1] # SE
         neighbors.each do |n|
-          @octomap[n] += 1 unless @octomap[n] == nil # || @flashers&.any? { |f| f == n  }
-          binding.pry if @flashers&.any? { |f| f == k  }
+          inc(n) unless @octomap[n] == nil
         end
-        show
       end
-      if @flashers # 0 if flashed
-        @flashers.each do |f|
-          @octomap[f] = 0
-          # show
-        end
-        @flashers = []
-      end
+      flashed += flashers
+      @flashcount += flashers.size
+    end
+    flashed.each do |f|
+      @octomap[f] = 0
     end
   end
   def flash(pos)
     @flashers << pos
     @octomap[pos] = -9
-    @flashcount += 1
   end
-  def inc(k,v)
-    @octomap[k] = (v + 1) unless @flashers&.any? { |f| f == k  }
-    binding.pry if @flashers&.any? { |f| f == k  }
+  def inc(k)
+    @octomap[k] = (@octomap[k] + 1)
   end
 end
 # --- Part One ---
 om = Octomap.new(INPUT)
 STEPCOUNT.times do |t|
-  om.show(t)
   om.step
+  om.show(t+1)
 end
-puts om.flashcount
+puts "#{om.flashcount} flashes"
 
 # --- Part Two ---
 
