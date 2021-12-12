4bf496b5cd74d6004b1940d0dbd97d35bbba1666
TyJaYo <tylerjamesyoung@gmail.com>
2021-12-11 21:18:53 -0600

    working day 11 part 2

--- 2021-day-11.rb
+++ 2021-day-11.rb
@@ -1,18 +1,20 @@
 #!/usr/bin/env ruby
-require 'pry'
 
 INPUT = ["8261344656","7773351175","7527856852","1763614673","8674556743","6853382153","4135852388","2846715522","7477425863","4723888888"]
 SINPUT = ["5483143223","2745854711","5264556173","6141336146","6357385478","4167524645","2176841721","6882881134","4846848554","5283751526"]
 ROWSIZE = INPUT.first.size
-ROWCOUNT = INPUT.size
-SLEEPYTIME = 0.1
-STEPCOUNT = 100
+ROWCOUNT = INPUT.size #part2
+OCTOTOTAL = ROWSIZE * ROWCOUNT #part2
+SLEEPYTIME = 0.07
+STEPCOUNT = 300
 CSI = "\e["
 
 class Octomap
   attr_reader :flashcount
+  attr_reader :flashsize #part2
   def initialize(stringlines)
     @flashcount = 0
+    @flashsize = 0
     @octomap = {}
     stringlines.each_with_index do |string,rownum|
       string.each_char.with_index do |c,i|
@@ -20,7 +22,7 @@ class Octomap
       end
     end
   end
-  def show(t='p')
+  def show(t)
     $stdout.write "#{CSI}2J"    # clear screen
     $stdout.write "#{CSI}1;1H"  # move to top left corner
     counter = ROWSIZE
@@ -40,7 +42,7 @@ class Octomap
     @octomap.each_key { |k| inc(k) } # all values += 1
     flashed = []
     while 'Tyler' > 'Everyone'
-      flashers = @octomap.select { |k, v| v > 9 && !flashed.include?(k)} .keys  # flash if > 9 and haven't flashed
+      flashers = @octomap.select { |k, v| v > 9 && !flashed.include?(k)} .keys # flash if > 9 and haven't flashed
       break if flashers.empty?
       flashers.each do |f| # adjacent to flash += 1
         neighbors = []
@@ -58,6 +60,7 @@ class Octomap
       end
       flashed += flashers
       @flashcount += flashers.size
+      @flashsize = flashed.size #part2
     end
     flashed.each do |f|
       @octomap[f] = 0
@@ -65,19 +68,20 @@ class Octomap
   end
   def flash(pos)
     @flashers << pos
-    @octomap[pos] = -9
   end
   def inc(k)
     @octomap[k] = (@octomap[k] + 1)
   end
 end
-# --- Part One ---
+
 om = Octomap.new(INPUT)
 STEPCOUNT.times do |t|
-  om.step
+  if om.flashsize == OCTOTOTAL #part2
+    om.show(t)
+    break
+  end
   om.show(t+1)
+  om.step
 end
 puts "#{om.flashcount} flashes"
-
-# --- Part Two ---
 
