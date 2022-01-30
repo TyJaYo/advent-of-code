5a08f8326fb929f653e707fdf5e4c7e614525c5f
TyJaYo <tylerjamesyoung@gmail.com>
2022-01-29 23:53:13 -0600

    day 3 part 2 wrong answer

--- 2020/2020-day-03.rb
+++ 2020/2020-day-03.rb
@@ -4,7 +4,7 @@ INPUT = ["....#............#.###...#.#.#.",".#.#....##.........#.....##.#..",".#
 
 # --- Day 3: Toboggan Trajectory ---
 
-# --- Part 1: Count Hit Trees ---
+puts "--- Part 1: Count Hit Trees ---"
 RIGHT = 3
 DOWN = 1
 aput = INPUT.map(&:chars)
@@ -18,4 +18,19 @@ end
 
 puts "On a slope of right #{RIGHT}, down #{DOWN}, you hit #{hits} trees."
 
-# --- Part 2:  ---
+puts "--- Part 2: Check Multiple Slopes ---"
+SLOPES_TO_CHECK = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+all_hits = []
+SLOPES_TO_CHECK.each do |slope|
+  aput = INPUT.map(&:chars)
+  right, down = slope
+  dx = 0
+  hits = 0
+  for t in (1...aput.size).step(down) do
+    dx += right
+    hits += 1 if aput[t].rotate(dx).first == '#'
+  end
+  all_hits << hits
+  puts "On a slope of right #{right}, down #{down}, you hit #{hits} trees."
+end
+puts "Multiplied together, that's #{all_hits.reduce(:*)}."
