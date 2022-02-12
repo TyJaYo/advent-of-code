ca8ed319fed3802eded1af9c2f04457127d05a56
TyJaYo <tylerjamesyoung@gmail.com>
2022-02-12 16:20:28 -0600

    Solve day 4 part 1
    Got a working solution to part 1. L11 chains splits and then uses the
    last split to form key/value pairs. That's probably a little much for
    one line to handle, but I also find it oddly readable strung together
    like that.

--- 2020/2020-day-04.rb
+++ 2020/2020-day-04.rb
@@ -1,7 +1,20 @@
 #!/usr/bin/env ruby
 
-INPUT = []
+puts "--- Day 4: Passport Processing ---"
 
-# --- Part One ---
+puts "--- Part 0: Parse Input ---"
+# PATH = './inputs/day-04-example.txt'
+PATH = './inputs/day-04.txt'
+INPUT = File.read(PATH).freeze
+puts "Successfully read input from #{PATH}" if INPUT.size > 0
+
+passports = INPUT.gsub("\n",' ').split('  ').map { |x| x.split }.map { |x| Hash[x.map { |x| x.split(':') }] }
+
+puts "--- Part 1: Count Valid by Field Presence ---"
+REQ_FIELDS = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
+
+valid_count = passports.count { |pp| REQ_FIELDS.all? { |k| pp.key?(k) } }
+
+puts "I count #{valid_count} valid passports."
 
 # --- Part Two ---
