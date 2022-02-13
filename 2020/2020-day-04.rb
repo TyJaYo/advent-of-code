dadf5f419429b88e96ec8a1ecdedb8bbb3b73463
TyJaYo <tylerjamesyoung@gmail.com>
2022-02-13 01:22:37 -0600

    Solve day 4 part 2
    Definitely not the most readable, but I think this is fairly efficient.

--- 2020/2020-day-04.rb
+++ 2020/2020-day-04.rb
@@ -10,11 +10,38 @@ puts "Successfully read input from #{PATH}" if INPUT.size > 0
 
 passports = INPUT.gsub("\n",' ').split('  ').map { |x| x.split }.map { |x| Hash[x.map { |x| x.split(':') }] }
 
-puts "--- Part 1: Count Valid by Field Presence ---"
-REQ_FIELDS = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
+puts "--- Part 1: Count Passports with All Required Fields ---"
+REQ_FIELDS = %w[byr iyr eyr hgt hcl ecl pid]
 
-valid_count = passports.count { |pp| REQ_FIELDS.all? { |k| pp.key?(k) } }
+pps_with_all_req_fields = passports.filter { |pp| REQ_FIELDS.all? { |k| pp.key?(k) } }
 
-puts "I count #{valid_count} valid passports."
+puts "I count #{pps_with_all_req_fields.count} passports with all required fields."
 
-# --- Part Two ---
+puts "--- Part 2: Count Fully Valid ---"
+LEGIT_ECLS = %w[amb blu brn gry grn hzl oth]
+
+def valid?(k,v)
+  case k
+  when "byr"
+    v.to_i.between?(1920,2002)
+  when "iyr"
+    v.to_i.between?(2010,2020)
+  when "eyr"
+    v.to_i.between?(2020,2030)
+  when "hgt"
+    num, u = v.match(/(\d+)([ci][mn])/)&.captures
+    (u == "cm" && num.to_i.between?(150,193)) || (u == "in" && num.to_i.between?(59,76))
+  when "hcl"
+    v.match?(/^#[\da-f]{6}$/)
+  when "ecl"
+    LEGIT_ECLS.any? { |ecl| ecl == v }
+  when "pid"
+    v.match?(/^\d{9}$/)
+  end
+end
+
+pps_with_all_req_fields_valid = pps_with_all_req_fields.filter { |pp|
+  REQ_FIELDS.all? { |f| valid?(f,pp[f]) }
+}
+
+puts "I count #{pps_with_all_req_fields_valid.count} passports with all required fields valid."
