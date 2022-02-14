78af7f7da25603010c7969d87449e216daac7c12
TyJaYo <tylerjamesyoung@gmail.com>
2022-02-14 09:30:00 -0600

    Initialize day 5 with input file
    Using a file reader again. Had to chomp each line and I suspect there's
    a more efficient way to do that.

---
+++ 2020/2020-day-05.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+puts "--- Day 5: Binary Boarding ---"
+
+puts "--- Part 0: Parse Input ---"
+PATH = './inputs/day-05.txt'
+INPUT = File.open(PATH).readlines.map(&:chomp).freeze
+
+puts "Successfully read input from #{PATH}" if INPUT
+
+puts "--- Part 1: Find Highest Seat ID ---"
+
+
+# puts "The highest Seat ID scanned was #{sids.max}."
+
