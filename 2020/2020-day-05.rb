f153d58adc9a5dfc8d3e2fff48d99f6fbf4abe07
TyJaYo <tylerjamesyoung@gmail.com>
2022-02-21 17:57:21 -0600

    Solves Day 5 Part 1

--- 2020/2020-day-05.rb
+++ 2020/2020-day-05.rb
@@ -10,6 +10,44 @@ puts "Successfully read input from #{PATH}" if INPUT
 
 puts "--- Part 1: Find Highest Seat ID ---"
 
+ROWS = (0..127)
+COLS = (0..7)
 
-# puts "The highest Seat ID scanned was #{sids.max}."
+def find_seat(sbin="BBFFBBFRLL".chars) # assumption: will recieve array
+  robary = sbin.shift(7) # assumption: array will start with 7 F/R chars
+  cobary = sbin # assumption: removing those 7 chars will leave 3 L/R chars
+  found_row = find("row",robary)
+  found_col = find("col",cobary)
+  seat_id = (found_row * 8) + found_col
+end
+
+def find(param,ary)
+  case param
+  when "row"
+    range = ROWS
+    upper, lower = "B", "F"
+  when "col" then range = COLS
+    range = COLS
+    upper, lower = "R", "L"
+  end
+  max = range.max
+  min = range.min
+  until range.size == 1
+    mid = (max + min)/2
+    dir = ary.shift
+    case dir
+    when upper then min = mid + 1
+    when lower then max = mid
+    end
+    range = (min..max)
+  end
+  range.max
+end
+
+sids = []
+INPUT.each do |pass|
+  sids << find_seat(pass.chars)
+end
+
+puts "The highest Seat ID scanned was #{sids.max}."
 
