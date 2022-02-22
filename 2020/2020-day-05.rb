9e3a4fda252c13a3947c2250373554bf2687fc50
TyJaYo <tylerjamesyoung@gmail.com>
2022-02-21 18:13:02 -0600

    Solves Day 5 Part 2
    Not especially performant, but it does the trick!

--- 2020/2020-day-05.rb
+++ 2020/2020-day-05.rb
@@ -13,9 +13,9 @@ puts "--- Part 1: Find Highest Seat ID ---"
 ROWS = (0..127)
 COLS = (0..7)
 
-def find_seat(sbin="BBFFBBFRLL".chars) # assumption: will recieve array
-  robary = sbin.shift(7) # assumption: array will start with 7 F/R chars
-  cobary = sbin # assumption: removing those 7 chars will leave 3 L/R chars
+def find_seat(sbin="BBFFBBFRLL".chars)
+  robary = sbin.shift(7)
+  cobary = sbin
   found_row = find("row",robary)
   found_col = find("col",cobary)
   seat_id = (found_row * 8) + found_col
@@ -44,10 +44,30 @@ def find(param,ary)
   range.max
 end
 
-sids = []
+@sids = []
 INPUT.each do |pass|
-  sids << find_seat(pass.chars)
+  @sids << find_seat(pass.chars)
 end
 
-puts "The highest Seat ID scanned was #{sids.max}."
+puts "The highest Seat ID scanned was #{@sids.max}."
 
+puts "--- Part 2: Find Missing Seat ID with Neighbors ---"
+
+all_sids = []
+ROWS.each do |row|
+  COLS.each do |col|
+    all_sids << (row * 8) + col
+  end
+end
+
+def is_empty?(sid)
+  !@sids.include?(sid)
+end
+
+def has_neighbors?(sid)
+  @sids.include?(sid - 1) && @sids.include?(sid + 1)
+end
+
+all_sids.each do |sid|
+  puts sid if is_empty?(sid) && has_neighbors?(sid)
+end
