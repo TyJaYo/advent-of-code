7a5c362d89adedbe76f7bcac5842c2b7444b8d3f
TyJaYo <tylerjamesyoung@gmail.com>
2022-03-01 21:00:37 -0600

    Solve Day 7 part 1
    Inefficient, but it got the right answer and didn't take forever.

--- 2020/2020-day-07.rb
+++ 2020/2020-day-07.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
+require "pry"
 puts "--- Day 7: Handy Haversacks ---"
 puts "--- Part 0: Parse Input ---"
 PATH = './inputs/day-07.txt'
 INPUT = File.open(PATH).readlines.map(&:chomp).freeze
 puts "Successfully read input from #{PATH}" if INPUT
 
-rules = {}
+@rules = {}
 INPUT.each do |line|
   container, contained = line.split(" bags contain ")
   contained = contained.scan(/(\d+.*?)bag/)
@@ -15,10 +16,31 @@ INPUT.each do |line|
   contained.each do |count_and_color|
     count_and_color[0] = count_and_color[0].to_i
   end
-  rules[container] = contained
+  @rules[container] = contained
 end
-
-# puts rules.first.inspect => ["bright indigo", [[4, "shiny turquoise"], [3, "wavy yellow"]]]
+# puts @rules.first.inspect => ["bright indigo", [[4, "shiny turquoise"], [3, "wavy yellow"]]]
 
 puts "--- Part 1: Count Unique Bag Colors Holding Shiny Gold Bags ---"
+def drill(cont)
+  color = cont.last
+  if color == "shiny gold"
+    @chain.each do |link|
+      @gold_holders << link
+    end
+  end
+  @rules[color].each do |cont|
+    drill(cont)
+  end
+end
+
+@gold_holders = []
+@rules.each_pair do |container, contained|
+  next if @gold_holders.include? container
+  @chain = [container]
+  contained.each do |cont|
+    drill(cont)
+  end
+end
+
 puts "How many bag colors can eventually contain at least one shiny gold bag?"
+puts @gold_holders.uniq.count
