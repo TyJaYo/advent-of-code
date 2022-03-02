3352df4aed12ac5d16933307dde92621ee5ce79f
TyJaYo <tylerjamesyoung@gmail.com>
2022-03-01 21:33:32 -0600

    Attempt Day 7 Part 2
    It doesn't work. I'm not multiplying or adding the right things at the right times.

--- 2020/2020-day-07.rb
+++ 2020/2020-day-07.rb
@@ -21,7 +21,7 @@ end
 # puts @rules.first.inspect => ["bright indigo", [[4, "shiny turquoise"], [3, "wavy yellow"]]]
 
 puts "--- Part 1: Count Unique Bag Colors Holding Shiny Gold Bags ---"
-def drill(cont)
+def drill_for_gold(cont)
   color = cont.last
   if color == "shiny gold"
     @chain.each do |link|
@@ -29,7 +29,7 @@ def drill(cont)
     end
   end
   @rules[color].each do |cont|
-    drill(cont)
+    drill_for_gold(cont)
   end
 end
 
@@ -38,9 +38,33 @@ end
   next if @gold_holders.include? container
   @chain = [container]
   contained.each do |cont|
-    drill(cont)
+    drill_for_gold(cont)
   end
 end
 
 puts "How many bag colors can eventually contain at least one shiny gold bag?"
 puts @gold_holders.uniq.count
+
+
+puts "--- Part 2: Count Bags inside a Shiny Gold Bag ---"
+
+def drill(cont)
+  count = cont.first
+  color = cont.last
+  @rules[color].each do |cont|
+    @multiplier = @multiplier * count
+    drill(cont)
+  end
+end
+
+@bag_count = 0
+golden_rule = @rules["shiny gold"]
+golden_rule.each do |cont|
+  @multiplier = cont.first
+  drill(cont)
+  @bag_count += @multiplier
+end
+# binding.pry
+
+puts "How many individual bags are required inside your single shiny gold bag?"
+puts @bag_count
