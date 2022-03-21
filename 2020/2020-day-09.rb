ee5bdc600d80c5e3e3811b10787c7dd9b1077240
TyJaYo <tylerjamesyoung@gmail.com>
2022-03-21 01:46:53 -0500

    Solve Day 9 Part 2
    Tried recursion again but failed so I backed it out and came up with this.
    Not at all optimal, but I'm glad to have gotten the answer!

--- 2020/2020-day-09.rb
+++ 2020/2020-day-09.rb
@@ -5,12 +5,14 @@ PATH = './inputs/day-09.txt'
 INPUT = File.open(PATH).readlines.map(&:chomp).map(&:to_i).freeze
 puts "Successfully read input from #{PATH}" if INPUT
 
-puts "--- Part 1: Find Combo Breaker ---"
+puts "--- Part 1: Find combo breaker ---"
 class Crawler
   attr_accessor :bloc
   def initialize
     @range = 25
     @found = false
+    @scopemin = 0
+    @scopemax = 1
   end
   def check(pos)
     bmin = pos - @range
@@ -24,7 +26,6 @@ class Crawler
         return if c + b == val
       end
     end
-    puts "No pair found in preceding #{@range} for #{val}."
     @found = val
   end
   def crawl
@@ -32,17 +33,32 @@ class Crawler
       check(pos)
       break if @found
     end
+    @found
+  end
+  def check_scope_sum(num)
+    scope = INPUT[@scopemin..@scopemax]
+    sum = scope.sum
+    case sum <=> num
+    when -1 then @scopemax += 1
+    when 0 then @found = scope.min + scope.max
+    when 1 then @scopemin += 1
+    end
+  end
+  def scan_for(num)
+    @found = false
+    until @found
+      check_scope_sum(num)
+    end
+    return @found
   end
 end
 
-part_one = Crawler.new
+crwlr = Crawler.new
 
 puts "Find the first number in the list which is not the sum of two of the 25 numbers before it."
 puts "What is the first number that does not have this property?"
-part_one.crawl
+puts ans_one = crwlr.crawl
 
 puts "--- Part 2: Find contiguous nums that sum to part 1 ---"
-part_two = Crawler.new
-
 puts "What is the encryption weakness in your XMAS-encrypted list of numbers?"
-# puts part_two.switch_fix
+puts ans_two = crwlr.scan_for(ans_one)
