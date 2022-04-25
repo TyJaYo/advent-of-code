009b59b76d2009098f10f09ab81c7d13074b028c
TyJaYo <tylerjamesyoung@gmail.com>
2022-04-25 00:10:49 -0500

    Add coordinates map as a hash

--- 2020/2020-day-11.rb
+++ 2020/2020-day-11.rb
@@ -3,36 +3,31 @@ require "pry"
 
 puts "--- Day 11: Seating System ---"
 puts "--- Part 0: Parse Input ---"
+
 # PATH = './inputs/day-11.txt'
 PATH = './inputs/day-11-example.txt'
-INPUT = File.open(PATH).readlines.map(&:chomp).map(&:chars).freeze
-puts "Successfully read input from #{PATH}" if INPUT
-SLEEPYTIME = 0.07
-CSI = "\e["
+MATRIX = File.open(PATH).readlines.map(&:chomp).map(&:chars).freeze
+puts "Successfully read input from #{PATH}" if MATRIX
 
-puts "--- Part 1: Simulate Life until changes cease ---"
+ROWSIZE = MATRIX.first.length
+COLSIZE = MATRIX.length
+
+puts "--- Part 1: Simulate Life until it doesn't change ---"
 class SeatMapper
   def initialize
-    @map = INPUT.dup
+    @mapped_matrix = {}
+    COLSIZE.times do |ct|
+      ROWSIZE.times do |rt|
+        @mapped_matrix[[ct,rt]] = MATRIX[ct][rt]
+      end
+    end
+
     @change = false
+    @step_counter = 0
   end
 
   def run
-    show_map(1)
     binding.pry
-  end
-
-  def show_map(t)
-    $stdout.write "#{CSI}2J"    # clear screen
-    $stdout.write "#{CSI}1;1H"  # move to top left corner
-    @map.each do |row|
-      row.each do |char|
-        $stdout.print char
-      end
-      $stdout.print "\n"
-    end
-    puts "After #{t} steps..."
-    sleep(SLEEPYTIME)
   end
 end
 
