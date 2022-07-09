5ba2de49ef87bc55f1d59d32afc8d9789aac1132
TyJaYo <tylerjamesyoung@gmail.com>
2022-07-09 17:52:52 -0500

    Day 12 Part 2 solved

--- 2020/2020-day-12.rb
+++ 2020/2020-day-12.rb
@@ -7,70 +7,70 @@ PATH = './inputs/day-12.txt'
 INSTRUCTIONS = File.open(PATH).readlines.map(&:chomp).freeze
 puts "Successfully read input from #{PATH}" if INSTRUCTIONS
 
-puts "--- Part 1: Take directions ---"
-class NavigatorP1
-  def initialize
-    @heading = 90 # east
-    @x = 0
-    @y = 0
-  end
+# puts "--- Part 1: Take directions ---"
+# class NavigatorP1
+#   def initialize
+#     @heading = 90 # east
+#     @x = 0
+#     @y = 0
+#   end
 
-  def run
-    follow_directions
-    output_manhattan
-  end
+#   def run
+#     follow_directions
+#     output_manhattan
+#   end
 
-  def follow_directions
-    INSTRUCTIONS.each do |i|
-      letter = i.slice! 0
-      number = i.to_i
-      case letter
-      when "N" then dy(number)
-      when "E" then dx(number)
-      when "S" then dy(-number)
-      when "W" then dx(-number)
-      when "R" then dh(number)
-      when "L" then dh(-number)
-      when "F" then go(number)
-      end
-    end
-  end
+#   def follow_directions
+#     INSTRUCTIONS.each do |i|
+#       letter = i.slice! 0
+#       number = i.to_i
+#       case letter
+#       when "N" then dy(number)
+#       when "E" then dx(number)
+#       when "S" then dy(-number)
+#       when "W" then dx(-number)
+#       when "R" then dh(number)
+#       when "L" then dh(-number)
+#       when "F" then go(number)
+#       end
+#     end
+#   end
 
-  def go(n)
-    reorient unless @heading.between?(0,359)
-    case @heading
-    when 0 then dy(n)
-    when 90 then dx(n)
-    when 180 then dy(-n)
-    when 270 then dx(-n)
-    end
-  end
+#   def go(n)
+#     reorient unless @heading.between?(0,359)
+#     case @heading
+#     when 0 then dy(n)
+#     when 90 then dx(n)
+#     when 180 then dy(-n)
+#     when 270 then dx(-n)
+#     end
+#   end
 
-  def reorient
-    @heading = 360 - @heading.abs while @heading < 0
-    @heading = @heading - 360 while @heading >= 360
-  end
+#   def reorient
+#     @heading = 360 - @heading.abs while @heading < 0
+#     @heading = @heading - 360 while @heading >= 360
+#   end
 
-  def dy(n)
-    @y += n
-  end
+#   def dy(n)
+#     @y += n
+#   end
 
-  def dx(n)
-    @x += n
-  end
+#   def dx(n)
+#     @x += n
+#   end
 
-  def dh(n)
-    @heading += n
-  end
+#   def dh(n)
+#     @heading += n
+#   end
 
-  def output_manhattan
-    puts "What is the Manhattan distance between that location and the ship's starting position?"
-    puts  "|#{@x}| + |#{@y}| = #{@x.abs} + #{@y.abs} = #{@x.abs + @y.abs}"
-  end
-end
+#   def output_manhattan
+#     puts "What is the Manhattan distance between that location and the ship's starting position?"
+#     puts  "|#{@x}| + |#{@y}| = #{@x.abs} + #{@y.abs} = #{@x.abs + @y.abs}"
+#   end
+# end
 
-n = NavigatorP1.new
-n.run
+# n = NavigatorP1.new
+# n.run
 
 puts "--- Part 2: Move waypoint ---"
 class NavigatorP2
@@ -121,6 +121,7 @@ class NavigatorP2
     @qs.rotate! until @qs.first == cq
     turns = n / 90
     @qs.rotate!(-turns)
+    @wx, @wy = @wy, @wx if turns.odd?
     set_quadrant(@qs.first)
   end
 
