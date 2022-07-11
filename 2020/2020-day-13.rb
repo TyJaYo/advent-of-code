e113354461075f7ce741fe5757d8fe04496e947b
TyJaYo <tylerjamesyoung@gmail.com>
2022-07-10 20:23:46 -0500

    Day 13 Part 1 solved

--- 2020/2020-day-13.rb
+++ 2020/2020-day-13.rb
@@ -3,7 +3,7 @@
 puts "--- Day 13: Shuttle Search ---"
 puts "--- Part 0: Parse Input ---"
 
-PATH = './inputs/day-12.txt'
+PATH = './inputs/day-13.txt'
 INPUT = File.open(PATH).readlines.map(&:chomp).freeze
 puts "Successfully read input from #{PATH}" if INPUT
 
@@ -12,31 +12,44 @@ class BusFinder
   def initialize
     @input = INPUT.dup
     @earliest = 0
-    @winner = 0
-    @wait = 0
     @buses = []
+    @bus_waits = []
+    @winner = 0
+    @winning_wait = 0
   end
 
   def run
     parse_input
-    calculate_winner
+    calculate_times
+    determine_winner
     report
   end
 
   def parse_input
     @earliest = @input.first.to_i
     @buses = @input.last.split(',').reject { |x| x == 'x' }.map(&:to_i)
-    puts @buses.inspect
   end
 
-  def calculate_winner
+  def calculate_times
+    @buses.each do |bus|
+      etd = bus
+      until etd >= @earliest
+        etd += bus
+      end
+      wait = etd - @earliest
+      @bus_waits << [wait, bus]
+    end
+  end
 
+  def determine_winner
+    @bus_waits.sort!
+    @winning_wait, @winner = @bus_waits[0][0], @bus_waits[0][1]
   end
 
   def report
     puts "What is the ID of the earliest bus you can take to the airport multiplied
     by the number of minutes you'll need to wait for that bus?"
-    puts  "#{@winner} ⨉ #{@wait} = #{@winner * @wait}"
+    puts  "#{@winner} ⨉ #{@winning_wait} = #{@winner * @winning_wait}"
   end
 end
 
