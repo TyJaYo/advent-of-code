0da9244da7a47bfa87b9203d65c8092cc820fc24
TyJaYo <tylerjamesyoung@gmail.com>
2022-07-16 17:18:06 -0500

    Day 14 solve Part 1

--- 2020/2020_day_14.rb
+++ 2020/2020_day_14.rb
@@ -15,10 +15,13 @@ puts '--- Part 1: initialization program ---'
 class BitMasker
   def initialize
     @inputs = INPUT.dup
+    @memory = []
+    @mask = Array.new(36, 'X')
   end
 
   def run
     massage_inputs
+    process_inputs
     output_sum
   end
 
@@ -34,12 +37,43 @@ class BitMasker
 
   def massage_mem(dex)
     @inputs[dex][0], address = @inputs[dex][0].match(/(mem)\[(\d+)\]/).captures
-    @inputs[dex][1] = [address.to_i, @inputs[dex][1].to_i]
+    @inputs[dex][1] = [address.to_i, expand_bits(@inputs[dex][1].to_i)]
+  end
+
+  def expand_bits(num)
+    ary = []
+    35.downto(0) do |bit|
+      dex = 35 - bit
+      ary[dex] = num[bit]
+    end
+    ary
+  end
+
+  def process_inputs
+    @inputs.each do |input|
+      case input[0]
+      when 'mask' then @mask = input[1]
+      else write_to_mem(input[1])
+      end
+    end
+  end
+
+  def write_to_mem(input)
+    address, bits = input[0], input[1]
+    bits = apply_mask(bits)
+    @memory[address] = bits.join.to_i(2)
+  end
+
+  def apply_mask(bits)
+    @mask.each_with_index do |maskbit, dex|
+      bits[dex] = maskbit unless maskbit == 'X'
+    end
+    bits
   end
 
   def output_sum
     puts 'What is the sum of all values left in memory after it completes?'
-    puts @inputs.inspect
+    puts @memory.compact.sum
   end
 end
 
