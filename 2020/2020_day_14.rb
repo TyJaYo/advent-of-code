d53bbefc8b5e0c170e36bf99f5075a31a0060a76
TyJaYo <tylerjamesyoung@gmail.com>
2022-07-17 19:04:11 -0500

    Day 14 Part 2 supercomputer solution

--- 2020/2020_day_14.rb
+++ 2020/2020_day_14.rb
@@ -1,6 +1,5 @@
-# frozen_string_literal: true
-
 # !/usr/bin/env ruby
+# frozen_string_literal: true
 
 puts '--- Day 14: Docking Data ---'
 
@@ -16,7 +15,7 @@ class BitMasker
   def initialize
     @inputs = INPUT.dup
     @memory = []
-    @mask = Array.new(36, 'X')
+    @mask = []
   end
 
   def run
@@ -43,8 +42,7 @@ class BitMasker
   def expand_bits(num)
     ary = []
     35.downto(0) do |bit|
-      dex = 35 - bit
-      ary[dex] = num[bit]
+      ary[35 - bit] = num[bit]
     end
     ary
   end
@@ -59,9 +57,8 @@ class BitMasker
   end
 
   def write_to_mem(input)
-    address, bits = input[0], input[1]
-    bits = apply_mask(bits)
-    @memory[address] = bits.join.to_i(2)
+    masked_bits = apply_mask(input[1])
+    @memory[input[0]] = masked_bits.join.to_i(2)
   end
 
   def apply_mask(bits)
@@ -79,3 +76,101 @@ end
 
 bm = BitMasker.new
 bm.run
+
+puts '--- Part 2: memory address decoder ---'
+# Class for solving Part 2
+class BitMaskerV2
+  def initialize
+    @inputs = INPUT.dup
+    @memory = []
+    @mask = []
+    @permutation_tables = []
+  end
+
+  def run
+    massage_inputs
+    process_inputs
+    output_sum
+  end
+
+  def massage_inputs
+    @inputs.each_with_index do |input, dex|
+      @inputs[dex] = input.split(' = ')
+      case @inputs[dex][0]
+      when 'mask' then @inputs[dex][1] = @inputs[dex][1].chars
+      else massage_mem(dex)
+      end
+    end
+  end
+
+  def massage_mem(dex)
+    @inputs[dex][0], address = @inputs[dex][0].match(/(mem)\[(\d+)\]/).captures
+    @inputs[dex][1] = [expand_bits(address.to_i), @inputs[dex][1].to_i]
+  end
+
+  def expand_bits(num)
+    ary = []
+    35.downto(0) do |bit|
+      ary[35 - bit] = num[bit]
+    end
+    ary
+  end
+
+  def process_inputs
+    @inputs.each do |input|
+      case input[0]
+      when 'mask' then @mask = construct_mask(input[1])
+      else write_to_mem(input[1])
+      end
+    end
+  end
+
+  def construct_mask(input)
+    input.each_with_index do |char, dex|
+      input[dex] = char.to_i unless char == 'X'
+    end
+    input
+  end
+
+  def write_to_mem(input)
+    masked_address = apply_mask(input[0])
+    permuted_addresses = permute(masked_address)
+    permuted_addresses.each do |address|
+      @memory[address.join.to_i(2)] = input[1]
+    end
+  end
+
+  def permute(address)
+    permuted_addresses = []
+    ex_count = address.count('X')
+    ex_dexes = address.each_index.select { |dex| address[dex] == 'X' }
+    add_permutation_table(ex_count) unless @permutation_tables[ex_count]
+    @permutation_tables[ex_count].each do |permutation|
+      address_permutation = address.dup
+      permutation.each_with_index do |value, dex|
+        address_permutation[ex_dexes[dex]] = value
+      end
+      permuted_addresses << address_permutation
+    end
+    permuted_addresses
+  end
+
+  def add_permutation_table(count)
+    @permutation_tables[count] = [0, 1].repeated_permutation(count).to_a
+  end
+
+  def apply_mask(bits)
+    @mask.each_with_index do |maskbit, dex|
+      bits[dex] = maskbit unless maskbit == 0
+    end
+    bits
+  end
+
+  def output_sum
+    puts 'What is the sum of all values left in memory after it completes?'
+    puts @memory.compact.sum
+  end
+end
+
+bm2 = BitMaskerV2.new
+bm2.run
