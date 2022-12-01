eef4ea63c4fdc6d7ffc3d76003fbe48ae09cf9c2
TyJaYo <tylerjamesyoung@gmail.com>
2022-12-01 12:03:06 -0700

    Solve Day 1

---
+++ 2022/2022_day_01.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+puts '--- Day 1: Calorie Counting ---'
+puts '--- Part 0: Parse Input ---'
+PATH = './inputs/day_01.txt'.freeze
+INPUT = File.open(PATH).readlines.map(&:to_i).freeze
+puts "Successfully read input from #{PATH}" if INPUT
+
+puts '--- Part 1: Find cal-max cargo ---'
+class CalFinder
+  def initialize
+    @elves = separate(INPUT.dup)
+  end
+
+  def separate(lines)
+    elves = []
+    elf = 0
+    lines.each do |line|
+      if line.positive?
+        elf += line
+      else
+        elves << elf
+        elf = 0
+      end
+    end
+    elves
+  end
+
+  def find_max
+    @elves.max
+  end
+
+  def top_three
+    @elves.sort!
+    puts @elves[-1] + @elves[-2] + @elves[-3]
+  end
+end
+
+cf = CalFinder.new
+puts cf.find_max
+puts '--- Part 2: Find cal-max top three ---'
+puts cf.top_three
