7a85322cefb47c9988a0a645e94f71d60337e38c
TyJaYo <tylerjamesyoung@gmail.com>
2022-12-10 01:51:58 -0600

    Solve Day 3

--- 2022/2022_day_03.rb
+++ 2022/2022_day_03.rb
@@ -5,24 +5,32 @@ PATH = './inputs/day_03.txt'.freeze
 INPUT = File.open(PATH).readlines.map { |l| l.split("\n") }.freeze
 puts "Successfully read input from #{PATH}" if INPUT
 
-puts '--- Part 1: Find sum of priorities ---'
+puts '--- Part 1: Find sum of item priorities ---'
 class RucksackParser
   def initialize
     @priority_map = PMap.new.zip
     @priority_sum = 0
-    @input = INPUT.dup
   end
 
-  def run
-    @input.each do |line|
+  def sack_check
+    INPUT.each do |line|
       intersection = find_dupe(line).first
       add_value(intersection)
     end
     report
   end
 
+  def badge_check
+    threes = INPUT.each_slice(3).to_a
+    threes.each do |triplet|
+      intersection = triplet[0][0].chars & triplet[1][0].chars & triplet[2][0].chars
+      add_value(intersection[0])
+    end
+    report
+  end
+
   def add_value(letter)
-    @priority_sum += @priority_map[letter] if @priority_map[letter]
+    @priority_sum += @priority_map[letter]
   end
 
   def find_dupe(line)
@@ -51,4 +59,8 @@ class PMap
 end
 
 rp = RucksackParser.new
-rp.run
+rp.sack_check
+
+puts '--- Part 2: Find sum of badge priorities ---'
+rp2 = RucksackParser.new
+rp2.badge_check
