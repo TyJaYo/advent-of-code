b32e1473858420e7dc3a75612e84ca057624b77b
TyJaYo <tylerjamesyoung@gmail.com>
2023-01-17 20:07:16 -0600

    Day 7 inscrutably incorrect

--- 2022/2022_day_07.rb
+++ 2022/2022_day_07.rb
@@ -1,14 +1,12 @@
 #!/usr/bin/env ruby
-require 'pry'
 PATH = './inputs/day_07.txt'.freeze
 INPUT = File.open(PATH).readlines.freeze
 puts "Successfully read input from #{PATH}" if INPUT
 
 class DrivMappr
   def initialize
-    @dirs = {}
-    @current_directory = 'root'
-    mkdir(INPUT.first)
+    @sizes = []
+    @current_directories = []
   end
 
   def run
@@ -18,59 +16,36 @@ class DrivMappr
 
   def process_input
     INPUT.each do |line|
-    binding.pry if @current_directory == nil
-      puts line
       case line
-      when /^\$ cd/
-        cd(line)
+      when /^\$ cd [^.]/
+        up_tally(line)
+      when /^\$ cd \./
+        down_tally
       when /^\d/
         mem(line)
-      when /^dir/
-        mkdir(line)
       end
     end
   end
 
-  def cd(line)
+  def up_tally(line)
     target = line[/\S+$/]
-    binding.pry if target == nil
-    @current_directory = if target == '..'
-    binding.pry if @dirs[@current_directory] == nil
-                           @dirs[@current_directory].parent
-                         else
-                           target
-                         end
-    puts "cd #{target} (#{@current_directory})"
-    puts @dirs
+    @current_directories << [target, 0]
   end
 
   def mem(line)
     filesize = line[/^\d+/]
-    @dirs[@current_directory].filesize += filesize.to_i
-  end
-
-  def mkdir(line)
-    dirname = line[/\S+$/]
-    if @dirs[dirname]
-      @dirs["#{dirname}-copy"] = Dir.new("#{dirname}-1", @current_directory)
-    else
-      @dirs[dirname] = Dir.new(dirname, @current_directory)
+    @current_directories.each do |directory|
+      directory[1] += filesize.to_i
     end
   end
 
-  def report
-    # puts INPUT
+  def down_tally
+    @sizes << @current_directories.pop
   end
-end
 
-class Dir
-  attr_accessor :dirname, :filesize, :parent
-
-  def initialize(dirname, cur_dir)
-    @dirname = dirname
-    @filesize = 0
-    binding.pry if cur_dir == nil
-    @parent = cur_dir
+  def report
+    candidates = @sizes.select { |size| size.last <= 100_000 }
+    puts candidates.map(&:last).sum
   end
 end
 
@@ -79,6 +54,6 @@ puts '--- Part 1 ---'
 dm = DrivMappr.new
 dm.run
 
-puts '--- Part 2 ---'
-dm2 = DrivMappr.new
-dm2.run
+# puts '--- Part 2 ---'
+# dm2 = DrivMappr.new
+# dm2.run
