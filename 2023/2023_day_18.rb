5ed5e403f89382ddc9a6988cba16b3b4c883a848
TyJaYo <tylerjamesyoung@gmail.com>
2023-12-19 00:41:40 -0600

    2023 Day 19 Part 1

---
+++ 2023/2023_day_18.rb
@@ -0,0 +1,85 @@
+#!/usr/bin/env ruby
+INPUT_PATH = './inputs/day_18.txt'
+INPUT = File.read(INPUT_PATH)
+
+class DayEighteen
+  def initialize
+    @flows = {}
+    @parts = []
+    @accepts = []
+    process(INPUT)
+  end
+
+  def process(raw)
+    raw_flows, raw_parts = raw.split("\n\n")
+    process_flows(raw_flows)
+    process_parts(raw_parts)
+  end
+
+  def process_flows(raw_flows)
+    raw_flows.split("\n").each do |line|
+      id, raw_steps = line.scan(/(\w+){(.*)}/)[0]
+      id = id.to_sym
+      @flows[id] = []
+      raw_steps.split(",").each do |step|
+        if step.match?(/^\w+$/)
+          dest = step
+        else
+          xmas, op, num, dest = step.scan(/(\w)?([<>])?(\d+)?:?(\w+)/)[0]
+        end
+        num = num.to_i unless num.nil?
+        @flows[id] << [xmas&.to_sym, op, num, dest.to_sym].compact
+      end
+    end
+  end
+
+  def process_parts(raw_parts)
+    raw_parts.split("\n").each do |part|
+      raw_attrs = part.delete("{}").split(",")
+      hashy = {}
+      raw_attrs.each do |raw_attr|
+        xmas, val = raw_attr.scan(/(\w)=(\d+)/)[0]
+        hashy[xmas.to_sym] = val.to_i
+      end
+      @parts << hashy
+    end
+  end
+
+  def sort_parts
+    @parts.each do |part|
+      work_flow(@flows[:in], part)
+    end
+  end
+
+  def work_flow(flow, part)
+    puts part.inspect
+    dest = nil
+    flow.each do |step|
+      puts step.inspect
+      dest ||= if step.size == 1
+        step[0]
+      elsif part[step[0]].method(step[1]).(step[2])
+        step[3]
+      end
+      break unless dest.nil?
+    end
+    @accepts << part if dest == :A
+    return if dest == :A || dest == :R
+    work_flow(@flows[dest], part)
+  end
+
+  def part_one
+    sort_parts
+    puts @accepts.sum { |a| a.sum { |_, val| val }}
+  end
+
+  def part_two
+  end
+end
+
+puts '--- Day 18 ---'
+day_eighteen = DayEighteen.new
+puts '--- Part 1 ---'
+day_eighteen.part_one
+puts '--- Part 2 ---'
+day_eighteen.part_two
