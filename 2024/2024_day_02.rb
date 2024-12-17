8f90825c0878f17d4b71383a9e80e8983c130601
TyJaYo <tylerjamesyoung@gmail.com>
2024-12-16 23:55:44 -0600

    Solve Day 2 Part 2

--- 2024/2024_day_02.rb
+++ 2024/2024_day_02.rb
@@ -8,97 +8,69 @@ puts "Successfully read input from #{PATH}" if INPUT
 
 class DayTwo
   def initialize
-    @reports = []
-    @safe = 0
+    @new_reports = []
+    @safe_reports = []
+    @unsafe_reports = []
     process(INPUT)
   end
 
   def process(lines)
     lines.each do |line|
       levels = line.split.map(&:to_i)
-      @reports << levels
+      @new_reports << levels
     end
   end
 
   def evaluate_safety(reports)
     reports.each do |levels|
-      @safe += 1 if consistent?(levels) && gradual?(levels)
+      if safe?(levels)
+        @safe_reports << levels
+      else
+        @unsafe_reports << levels
+      end
     end
   end
 
-  def consistent?(levels)
-    differences = levels.each_cons(2).map { |a, b| a - b }
-    differences.all?(&:positive?) || differences.all?(&:negative?)
-  end
+  def safe?(levels)
+    is_increasing_gradually = levels.each_cons(2).all? do |a, b|
+      a < b && (b - a).between?(1, 3)
+    end
+    return true if is_increasing_gradually
 
-  def gradual?(levels)
-    levels.each_cons(2).all? { |a, b| (a - b).abs.between?(1, 3) }
+    is_decreasing_gradually = levels.each_cons(2).all? do |a, b|
+      a > b && (a - b).between?(1, 3)
+    end
+
+    is_decreasing_gradually
   end
 
   def part_one
-    evaluate_safety(@reports)
-    puts "There are #{@safe} safe reports"
-    system("echo #{@safe} | pbcopy")
-    puts 'Copied to clipboard!'
+    evaluate_safety(@new_reports)
+    report(@safe_reports.count, 'Safe Reports')
+  end
+
+  def report(value, name = nil)
+    puts "#{name}: #{value}"
+    system("echo #{value} | pbcopy")
+    puts "Copied to clipboard!"
   end
 
   def part_two
-    @safe = 0
-    approximate_safety(@reports)
-    puts "There are #{@safe} reports that are more or less safe"
+    approximate_safety
+    report(@safe_reports.count, 'Safe Reports')
   end
 
-  def approximate_safety(reports)
-    reports.each do |levels|
-      @safe += 1 if more_or_less_consistent?(levels) && more_or_less_gradual?(levels)
+  def approximate_safety
+    @unsafe_reports.each do |levels|
+      levels.each_with_index do |_, index|
+        new_levels = levels.dup
+        new_levels.delete_at(index)
+        if safe?(new_levels)
+          @safe_reports << new_levels
+          break
+        end
+      end
     end
-  end
-
-  def more_or_less_consistent?(levels)
-    differences = levels.each_cons(2).map { |a, b| a - b }
-    positive_indices = []
-    negative_indices = []
-    differences.each_with_index do |diff, index|
-      positive_indices << (index + 1) if diff.positive?
-      negative_indices << (index + 1) if diff.negative?
-    end
-    if positive_indices.empty? || negative_indices.empty?
-      puts "#{levels} is consistent"
-      return true
-    end
-
-    if positive_indices.one? && consistent?(differences.reject.with_index { |_, index| index == positive_indices.first })
-      puts "#{levels} is more or less consistent after removing #{levels[positive_indices.first]}"
-      return true
-    end
-
-    if negative_indices.one? && consistent?(differences.reject.with_index { |_, index| index == negative_indices.first })
-      puts "#{levels} is more or less consistent after removing #{levels[negative_indices.first]}"
-      return true
-    end
-
-    false
-  end
-
-  def more_or_less_gradual?(levels)
-    deltas = levels.each_cons(2).map { |a, b| (a - b).abs }
-    small_indices = []
-    large_indices = []
-    deltas.each_with_index do |delta, index|
-      small_indices << index if delta.between?(1, 3)
-      large_indices << index if delta > 3
-    end
-    if large_indices.empty?
-      puts "#{levels} is gradual"
-      return true
-    end
-
-    if large_indices.one? && gradual?(levels.reject.with_index { |_, index| index == large_indices.first })
-      puts "#{levels} is more or less gradual after removing #{levels[large_indices.first]}"
-      return true
-    end
-
-    false
   end
 end
 
